9c0bb0376b03c8f2f70441ac7155f18ec275b990
Dilip <dilipchouhan153@gmail.com>
2017-01-10 19:15:26 +0530

    feat: Implemented routes in main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 /* global document:true */
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes/routes';
 
 ReactDOM.render(
-  <div>Hello World</div>,
+  <Router history={browserHistory}>
+    {routes}
+  </Router>,
   document.getElementById('root'),
 );
