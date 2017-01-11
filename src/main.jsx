22c742961b6b2b5bbb7812f3fb0fcdae57561ec6
Dilip <dilipchouhan153@gmail.com>
2017-01-11 17:42:12 +0530

    chore: run rootSaga when App starts

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes/routes';
 import store from './store/store';
+import runRootSaga from './sagas/rootSaga';
+
+runRootSaga();
 
 ReactDOM.render(
   <Provider store={store}>
