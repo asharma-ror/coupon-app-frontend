365ce88a2c6669bf958c2518fb2a70818abb148b
Dilip <dilipchouhan153@gmail.com>
2017-01-13 02:29:04 +0530

    chore: Created and implemented saga for dispatching token and user to store

--- src/modules/authentication/sagas/loginFlowSaga.js
+++ src/modules/authentication/sagas/loginFlowSaga.js
@@ -1,8 +1,13 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
-import { LOGIN_SUCCESS } from './actions/loginAction';
+import { put, takeEvery } from 'redux-saga/effects';
+import { LOGIN_SUCCESS, tokenAction } from '../actions/loginAction';
+import { userAction } from '../../user/actions/userActions';
 
-function* loginSuccess(action) {
-  yield put();
+export function* loginSuccess(action) {
+  const token = action.data.token;
+  yield put(tokenAction(token));
+
+  const user = action.data.user;
+  yield put(userAction(user));
 }
 
 export function* watchLogin() {
--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,10 +1,12 @@
 import { fork } from 'redux-saga/effects';
 import { sagaMiddleware } from '../store/store';
 import { watchGetApi, watchPostApi } from '../api/apiSaga';
+import { watchLogin } from '../modules/authentication/sagas/loginFlowSaga';
 
 function* root() {
   yield fork(watchGetApi);
   yield fork(watchPostApi);
+  yield fork(watchLogin);
 }
 
 const runRootSaga = () => sagaMiddleware.run(root);
