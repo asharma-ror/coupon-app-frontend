96411944cd25f5b9243bc4d069e66cdda075738d
Dilip <dilipchouhan153@gmail.com>
2017-01-12 12:25:34 +0530

    feat: Login Screen ui (WIP)

--- src/modules/authentication/screens/Login.jsx
+++ src/modules/authentication/screens/Login.jsx
@@ -1,7 +1,34 @@
 import * as React from 'react';
+// import style from './login.css';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => (
-  <div className="ui raised segment">
+  <div className={'body-bg'}>
+    <div className={'login-field'}>
+      <div className={'container'}>
+        <div className={'row'}>
+          <div className={'col-md-6 '}>
+            <div className={'panel panel-login'}>
+              <div className={'panel-heading'}>
+                <div className={'row'}>
+                  <div className={'col-xs-6 login'}>
+                    <a className={'active'} id={'login-form-link'}>Login</a>
+                  </div>
+                </div>
+                <hr />
+              </div>
+              <div className={'panel-body'}>
+                <div className={'row'}>
+                  <div className={'col-lg-12'}>
+                    <LoginForm />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
