aedbdfbb837d7b250751feec20e7d6cb469f72a3
Dilip <dilipchouhan153@gmail.com>
2017-01-13 22:55:29 +0530

    fix: Remove all data from redux store when logout

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import authentication from '../modules/authentication/reducers/authentication';
 import user from '../modules/user/reducers/user';
 import coupons from '../modules/coupons/reducers/coupons';
+import { LOGOUT } from '../modules/authentication/actions/loginAction';
 
 const reducers = {
   authentication,
@@ -9,4 +10,14 @@ const reducers = {
   coupons,
 };
 
-export default combineReducers(reducers);
+const appReducer = combineReducers(reducers);
+
+const rootReducer = (state, action) => {
+  if (action.type === LOGOUT) {
+    return undefined;
+  }
+
+  return appReducer(state, action);
+};
+
+export default rootReducer;
