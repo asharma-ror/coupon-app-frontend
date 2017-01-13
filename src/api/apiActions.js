da36046b97169707b313032d2d8afb557a054ac9
Dilip <dilipchouhan153@gmail.com>
2017-01-13 14:13:40 +0530

    feat: Added delete Api functionality

--- src/api/apiActions.js
+++ src/api/apiActions.js
@@ -1,5 +1,6 @@
 export const GET_API_ACTION = 'GET_API_ACTION';
 export const POST_API_ACTION = 'POST_API_ACTION';
+export const DELETE_API_ACTION = 'DELETE_API_ACTION';
 
 export const getApiAction = body => ({
   type: GET_API_ACTION,
@@ -8,5 +9,10 @@ export const getApiAction = body => ({
 
 export const postApiAction = body => ({
   type: POST_API_ACTION,
+  ...body,
+});
+
+export const deleteApiAction = body => ({
+  type: DELETE_API_ACTION,
   ...body,
 });
--- src/api/apiSaga.js
+++ src/api/apiSaga.js
@@ -1,25 +1,31 @@
 import { call, put, takeEvery } from 'redux-saga/effects';
-import { GET_API_ACTION, POST_API_ACTION } from './apiActions';
-import { getApiCall, postApiCall } from './api';
+import { GET_API_ACTION, POST_API_ACTION, DELETE_API_ACTION } from './apiActions';
+import { getApiCall, postApiCall, deleteApiCall } from './api';
+
+function* manageResponse(action, response) {
+  if (response.err) {
+    yield put({ type: action.types[2], data: response });
+  } else {
+    yield put({ type: action.types[1], data: response });
+  }
+}
 
 function* getApi(action) {
   yield put({ type: action.types[0] });
   const response = yield call(getApiCall, action.url, action.body);
-
-  if (response.error) {
-    yield put({ type: action.types[2], data: response });
-  }
-  yield put({ type: action.types[1], data: response });
+  yield call(manageResponse, action, response);
 }
 
 function* postApi(action) {
   yield put({ type: action.types[0] });
   const response = yield call(postApiCall, action.url, action.body);
-  if (response.error) {
-    yield put({ type: action.types[2], data: response });
-  }
+  yield call(manageResponse, action, response);
+}
 
-  yield put({ type: action.types[1], data: response });
+function* deleteApi(action) {
+  yield put({ type: action.types[0] });
+  const response = yield call(deleteApiCall, action.url, action.body);
+  yield call(manageResponse, action, response);
 }
 
 export function* watchGetApi() {
@@ -28,4 +34,8 @@ export function* watchGetApi() {
 
 export function* watchPostApi() {
   yield takeEvery(POST_API_ACTION, postApi);
+}
+
+export function* watchDeleteApi() {
+  yield takeEvery(DELETE_API_ACTION, deleteApi);
 }
