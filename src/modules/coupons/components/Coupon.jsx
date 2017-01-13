9c1a4ab49535cc1607549c0ea52fb17d913ae547
Dilip <dilipchouhan153@gmail.com>
2017-01-13 20:38:14 +0530

    chore: Modified ui and implemented functionality

--- src/modules/coupons/components/Coupon.jsx
+++ src/modules/coupons/components/Coupon.jsx
@@ -1,20 +1,34 @@
 import * as React from 'react';
 
-const Coupon = () => (
-  <li className={'col-md-4 col-xs-12 col-sm-4'}>
-    <a>
-      <div className={'left-block'}>
-        <span>Save 30%</span>
-        <p>Lorem ipsum sit amet</p>
+const Coupon = ({ coupon, showDelete, deleteCoupon }) => {
+  const { currency, id } = coupon;
+
+  const offTitle = coupon.amount_off ? `${coupon.amount_off}${currency}` : `${coupon.percent_off}%`;
+
+  return (
+    <li className={'col-md-4 col-xs-12 col-sm-6'}>
+      <div className={'main-white-bg'}>
+        <div className={'left-block'}>
+          <h4>Coupon Code :</h4>
+          <span>{id}</span>
+        </div>
+        <div className={'space-block'}>
+          <div className={'save-amount'}><h4>Save:</h4><p>{offTitle}</p></div>
+          <div className={'pull-left right-b'}>
+            <h4>{`Duration: ${coupon.duration}`}</h4>
+            <h4>{`Redeemed: ${coupon.times_redeemed}`}</h4>
+          </div>
+        </div>
       </div>
-      <div className={'pull-left right-b'}>
-        <h5>Coupon Timeline</h5>
-        <button className={'btn btn-primary'}>GIFT</button>
-        <p>Lorem ipsum sit amet</p>
-      </div>
-    </a>
-    <a className={'close-btn'}>x</a>
-  </li>
-);
+      { showDelete && <button className={'close-btn'} onClick={() => deleteCoupon(coupon.id)}>x</button>}
+    </li>
+  );
+};
+
+Coupon.propTypes = {
+  showDelete: React.PropTypes.bool.isRequired,
+  coupon: React.PropTypes.object.isRequired,
+  deleteCoupon: React.PropTypes.func.isRequired,
+};
 
 export default Coupon;
