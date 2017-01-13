c81025d98e9696998cdeec4a88faa8b9d84bc408
Dilip <dilipchouhan153@gmail.com>
2017-01-13 14:50:35 +0530

    feat: Store token in local storage

--- src/modules/authentication/sagas/loginFlowSaga.js
+++ src/modules/authentication/sagas/loginFlowSaga.js
@@ -1,13 +1,17 @@
+/* global localStorage:true*/
+/* eslint no-undef: "error"*/
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { LOGIN_SUCCESS, tokenAction } from '../actions/loginAction';
 import { userAction } from '../../user/actions/userActions';
 import { pushPath } from '../../../routes/actions/routeActions';
 import { COUPONS_ROUTE } from '../../../constants/Routes';
 import { setAuthorizationToken } from '../../../api/api';
+import { AUTHENTICATION_TOKEN_STORAGE_KEY } from '../../../constants/constants';
 
 export function* loginSuccess(action) {
   const token = action.data.token;
   yield put(tokenAction(token));
+  localStorage.setItem(AUTHENTICATION_TOKEN_STORAGE_KEY, token);
   yield call(setAuthorizationToken, token);
 
   const user = action.data.user;
