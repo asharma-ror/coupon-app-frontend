2519941a90f4ca1d8edf1dffa58d950b2e813c8f
Dilip <dilipchouhan153@gmail.com>
2017-01-13 10:59:16 +0530

    chore: Added push path saga to root saga

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,8 +2,10 @@ import { fork } from 'redux-saga/effects';
 import { sagaMiddleware } from '../store/store';
 import { watchGetApi, watchPostApi } from '../api/apiSaga';
 import { watchLogin } from '../modules/authentication/sagas/loginFlowSaga';
+import { watchPushPathSaga } from '../routes/sagas/routeSaga';
 
 function* root() {
+  yield fork(watchPushPathSaga);
   yield fork(watchGetApi);
   yield fork(watchPostApi);
   yield fork(watchLogin);
