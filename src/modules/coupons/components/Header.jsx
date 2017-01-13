e545815603d643a95d865dfd475e64c438a008a0
Dilip <dilipchouhan153@gmail.com>
2017-01-13 20:38:47 +0530

    chore: MOdified ui and implemented functionality

--- src/modules/coupons/components/Header.jsx
+++ src/modules/coupons/components/Header.jsx
@@ -1,29 +1,59 @@
 import * as React from 'react';
 import AddCouponForm from './AddCouponForm';
 
-const Header = () => (
-  <header>
-    <div className={'container'}>
-      <div className={'logo pull-left'}>
-        <h1>
-          <a>LOGO</a>
-        </h1>
-      </div>
-      <div className={'right-block pull-right'}>
-        <ul className={'dropdown'}>
-          <li className={'add-coupon'}>
-            <a className={'btn btn-default dropdown-toggle'} type={'button'} id={'dropdownMenu1'} data-toggle={'dropdown'}>Add Coupon</a>
-            <div className={'dropdown-menu custom-form'} role={'menu'} aria-labelledby={'dropdownMenu1'}>
-              <AddCouponForm />
-            </div>
-          </li>
-          <li>
-            <a className={'btn btn-default'}>Logout</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </header>
-);
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openForm: false,
+    };
+  }
+
+  closeForm() {
+    this.setState({ openForm: false });
+  }
+
+  render() {
+    return (
+      <header>
+        <div className={'container'}>
+          <div className={'logo pull-left'}>
+            <h1>
+              <a>LOGO</a>
+            </h1>
+          </div>
+          <div className={'right-block pull-right'}>
+            <ul className={'dropdown'}>
+              <li className={'add-coupon'}>
+                <button
+                  className={'btn btn-default dropdown-toggle'}
+                  onClick={() => this.setState({ openForm: true })}
+                >Add Coupon</button>
+                { this.state.openForm &&
+                  <div className={'dropdown-menu custom-form'} role={'menu'} aria-labelledby={'dropdownMenu1'}>
+                    <AddCouponForm
+                      createCoupon={this.props.createCoupon}
+                      closeForm={() => this.closeForm()}
+                    />
+                  </div>
+                }
+              </li>
+              <li>
+                <button className={'btn btn-default'} onClick={this.props.logout}>Logout</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  createCoupon: React.PropTypes.func.isRequired,
+  logout: React.PropTypes.func.isRequired,
+};
 
 export default Header;
