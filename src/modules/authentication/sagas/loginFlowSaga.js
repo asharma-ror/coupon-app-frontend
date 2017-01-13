8389e3a2276b1f5c92e575a634da9f94af75d4e6
Dilip <dilipchouhan153@gmail.com>
2017-01-13 14:14:51 +0530

    chore: Set authorization token for api calling

--- src/modules/authentication/sagas/loginFlowSaga.js
+++ src/modules/authentication/sagas/loginFlowSaga.js
@@ -1,12 +1,14 @@
-import { put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeEvery } from 'redux-saga/effects';
 import { LOGIN_SUCCESS, tokenAction } from '../actions/loginAction';
 import { userAction } from '../../user/actions/userActions';
 import { pushPath } from '../../../routes/actions/routeActions';
 import { COUPONS_ROUTE } from '../../../constants/Routes';
+import { setAuthorizationToken } from '../../../api/api';
 
 export function* loginSuccess(action) {
   const token = action.data.token;
   yield put(tokenAction(token));
+  yield call(setAuthorizationToken, token);
 
   const user = action.data.user;
   yield put(userAction(user));
