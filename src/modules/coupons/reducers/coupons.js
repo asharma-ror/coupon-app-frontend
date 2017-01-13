d660350c051ad9d3b4ac00226d14be9065aac0fa
Dilip <dilipchouhan153@gmail.com>
2017-01-13 14:17:33 +0530

    feat: Added coupon reducer

---
+++ src/modules/coupons/reducers/coupons.js
@@ -0,0 +1,63 @@
+import * as CouponActionTypes from '../actions/couponActionTypes';
+
+const initialState = {
+  isGettingCoupons: false,
+  isDeletingCoupons: false,
+  isCreatingCoupons: false,
+  coupons: [],
+};
+
+const coupons = (state = initialState, action) => {
+  switch (action.type) {
+    case CouponActionTypes.GET_COUPONS:
+      return {
+        ...state,
+        isGettingCoupons: true,
+      };
+    case CouponActionTypes.GET_COUPONS_SUCCESS:
+      return {
+        ...state,
+        isGettingCoupons: false,
+        coupons: action.data,
+      };
+    case CouponActionTypes.GET_COUPONS_FAILURE:
+      return {
+        ...state,
+        isGettingCoupons: false,
+      };
+    case CouponActionTypes.DELETE_COUPON:
+      return {
+        ...state,
+        isDeletingCoupons: true,
+      };
+    case CouponActionTypes.DELETE_COUPON_SUCCESS:
+      return {
+        ...state,
+        isDeletingCoupons: false,
+      };
+    case CouponActionTypes.DELETE_COUPON_FAILURE:
+      return {
+        ...state,
+        isDeletingCoupons: false,
+      };
+    case CouponActionTypes.CREATE_COUPON:
+      return {
+        ...state,
+        isCreatingCoupons: true,
+      };
+    case CouponActionTypes.CREATE_COUPON_SUCCESS:
+      return {
+        ...state,
+        isCreatingCoupons: false,
+      };
+    case CouponActionTypes.CREATE_COUPON_FAILURE:
+      return {
+        ...state,
+        isCreatingCoupons: false,
+      };
+    default:
+      return state;
+  }
+};
+
+export default coupons;
--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import authentication from '../modules/authentication/reducers/authentication';
 import user from '../modules/user/reducers/user';
+import coupons from '../modules/coupons/reducers/coupons';
 
 const reducers = {
   authentication,
   user,
+  coupons,
 };
 
 export default combineReducers(reducers);
