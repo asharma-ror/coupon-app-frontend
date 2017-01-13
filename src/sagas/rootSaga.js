9a167b5b2d39088e545edf2176b4808fe3dbd790
Dilip <dilipchouhan153@gmail.com>
2017-01-13 14:17:56 +0530

    chore: Added coupon saga to route

--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -2,6 +2,7 @@ import { fork } from 'redux-saga/effects';
 import { sagaMiddleware } from '../store/store';
 import { watchGetApi, watchPostApi } from '../api/apiSaga';
 import { watchLogin } from '../modules/authentication/sagas/loginFlowSaga';
+import { watchCouponsAction } from '../modules/coupons/sagas/couponsSaga';
 import { watchPushPathSaga } from '../routes/sagas/routeSaga';
 
 function* root() {
@@ -9,6 +10,7 @@ function* root() {
   yield fork(watchGetApi);
   yield fork(watchPostApi);
   yield fork(watchLogin);
+  yield fork(watchCouponsAction);
 }
 
 const runRootSaga = () => sagaMiddleware.run(root);
