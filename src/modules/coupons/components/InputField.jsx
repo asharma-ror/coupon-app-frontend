e9785642255506344a8e6d618920de1e931b5827
Dilip <dilipchouhan153@gmail.com>
2017-01-13 21:37:14 +0530

    chore: Added disabled props

--- src/modules/coupons/components/InputField.jsx
+++ src/modules/coupons/components/InputField.jsx
@@ -3,12 +3,15 @@ import * as React from 'react';
 const InputField = props => (
   <div className={'form-group'}>
     <label htmlFor={'formGroupExampleInput'}>{props.placeholder}</label>
-    <input type={'text'} className={'form-control'} {...props} />
+    <input disabled={props.disabled} type={'text'} className={'form-control'} {...props} />
   </div>
 );
 
 InputField.propTypes = {
   placeholder: React.PropTypes.string.isRequired,
+  disabled: React.PropTypes.bool,
 };
+
+InputField.defaultProps = { disabled: false };
 
 export default InputField;
